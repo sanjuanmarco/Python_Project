fd2025b0472d9ad19091b51056665c2bba9b21c8
corozanity <corozat@gmail.com>
2021-10-31 22:45:13 +0800

    FLASK COMPLETE FUNCTION
    Add similarity scores function - app. py for all related links
    Add validation to limit the file upload to jpeg, png - app.py def user_image
    Add validation for statement - "dsadsa dsadsa dsadas das dsa" inaaccept
    Create text for analysis - results pages
    Create text for similarity scores - katabi ng related links
    Create text for GUIDE - wrongURL.html

--- FLASK/app.py
+++ FLASK/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request
+import os
+
 app = Flask(__name__)
 
+twitter = "twitter"
+fb = "facebook"
 
 
 @app.route("/")
@@ -10,12 +14,14 @@ def home():
 
 #-----------------------------------------------------------FOR URL----------------------------------------------------------------
 import pickle
-import requests
-from newsplease import NewsPlease
+
 from googlesearch import search 
 
 @app.route("/URLResults", methods = ["GET", "POST"])
 def user():
+    import requests
+    from newsplease import NewsPlease
+
     if request.method == "POST":
         urlink = str(request.form.get("link"))
 
@@ -25,16 +31,14 @@ def user():
             head = article.title
 
             mylinks = []
-            twitter = "https://twitter.com"
-            fb = "https://www.facebook.com"
-            mfb = "https://m.facebook.com"
+            
 
             if twitter in urlink or fb in urlink:
                 msg = "Sorry, social media links cannot be accessed due to privacy laws. For social media posts, please copy the text and paste in the INSERT STATEMENT field."
                 return render_template("wrongURL.html", msg = msg)
             elif head != None:
                 #score
-                load_model = pickle.load(open('final_model.sav', 'rb'))
+                load_model = pickle.load(open('final_model__.sav', 'rb'))
                 prediction = load_model.predict([head])
                 prob = load_model.predict_proba([head])
 
@@ -45,17 +49,19 @@ def user():
 
                 #relatedlinks
                 for i in search(head, tld="co.in", num=5, start=1, stop=5, pause=2):
-                    if fb in i or mfb in i:
+                    if fb in i or twitter in i:
                         continue
                     mylinks.append(str(i))
 
-                return render_template("URLResults.html", links = urlink, rel = mylinks, pred = pred, prob = probs, head =head)
+                return render_template("URLResults.html", links = urlink, rel = mylinks, pred = pred, prob = probs, head = head)
+
+            
 
         except requests.ConnectionError as exception:
             msg = "URL does not exist, please make sure to provide a valid URL."
             return render_template("wrongURL.html", msg = msg)
 
-        except requests.exceptions.MissingSchema:
+        except Exception:
             msg = "URL not detected, please make sure to provide a valid URL."
             return render_template("wrongURL.html", msg = msg)
         
@@ -63,10 +69,98 @@ def user():
 
     
 
-#-----------------------------------------------------------FOR URL----------------------------------------------------------------
+#-----------------------------------------------------------FOR STATEMENT----------------------------------------------------------------
 
 
 
+@app.route("/StatementResults", methods = ["GET", "POST"])
+def user_statement():
+    
+    if request.method == "POST":
+
+        state = str(request.form.get("state"))
+
+        res = len(state.split())
+
+        if res < 4:
+            msg = "Not enough information, please provide a longer statement."
+            return render_template("wrongURL.html", msg = msg)
+
+        else:
+
+            #score
+            load_model = pickle.load(open('final_model__.sav', 'rb'))
+            prediction = load_model.predict([state])
+            prob = load_model.predict_proba([state])
+
+            pred = prediction[0]
+            probability = prob[0][1]
+
+            probs = round((probability*100))
+
+            mystate = []
+            #relatedlinks
+            for i in search(state, tld="co.in", num=5, start=1, stop=5, pause=2):
+                if fb in i or twitter in i:
+                    continue
+                mystate.append(str(i))
+        
+            return render_template("StatementResults.html", rel = mystate, state = state, pred = pred, prob = probs)
+
+#-----------------------------------------------------------FOR IMAGE----------------------------------------------------------------
+
+app.config["UPLOAD_PATH"] = "C:/Users/Kyle/Documents/SVM/static/uploads"
+@app.route("/ImageResults", methods = ["GET", "POST"])
+def user_image():
+
+    import easyocr
+    
+    
+    if request.method == "POST":
+        if request.files:
+
+            img = request.files["image_upload"]
+
+            print(img.filename)
+            
+            img_file = img.save(os.path.join(app.config['UPLOAD_PATH'], img.filename)) #save to local folder
+
+            image = "static/uploads/" + img.filename #location to pass in html
+
+
+            reader = easyocr.Reader(['en'], gpu=False)
+            result = reader.readtext(image, detail = 0) 
+
+            result_list = []
+
+            for word in result:
+                result_list.append(word)
+
+            sentences = " ".join(result_list)
+
+            if (not sentences):
+                msg = "No text detected, please check your uploaded image."
+                return render_template("wrongURL.html", msg = msg)
+
+            else:
+                #score
+                load_model = pickle.load(open('final_model__.sav', 'rb'))
+                prediction = load_model.predict([sentences])
+                prob = load_model.predict_proba([sentences])
+
+                pred = prediction[0]
+                probability = prob[0][1]
+
+                probs = round((probability*100))
+
+                mywords = []
+                #relatedlinks
+                for i in search(sentences, tld="co.in", num=5, start=1, stop=5, pause=2):
+                    if fb in i or twitter in i:
+                        continue
+                    mywords.append(str(i))
+        
+                return render_template("ImageResults.html", image = image, sentences = sentences, rel = mywords, pred = pred, prob = probs)
 
 if __name__ == '__main__': 
     app.run(debug = True)
